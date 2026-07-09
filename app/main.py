91ffd8cf60746f353b035ec04c49aab4efaa3c02
krishannair <krishanvnair2001@gmail.com>
2026-07-10 01:41:17 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -3,11 +3,11 @@ import sys
 
 def main():
     # TODO: Uncomment the code below to pass the first stage
-    while True:
+    command = ""
+    while command != "exit":
         sys.stdout.write("$ ")
         command = input()
         print(f"{command}: command not found")
-
 
 if __name__ == "__main__":
     main()
